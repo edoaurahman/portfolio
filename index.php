1c49bce9cba4624102bb0903daeaeda736b9387c
edoaurahman <edoaurahman@gmail.com>
2022-03-30 18:39:47 +0700

    add section id on navbar

--- index.php
+++ index.php
@@ -17,21 +17,21 @@
         <div class="container">
             <a class="navbar-brand" href="#">Edoaurahman</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link" href="#about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Projects</a>
+                        <a class="nav-link" href="#projects">Projects</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a class="nav-link" href="#contact">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -47,7 +47,9 @@
             <h1 class="display-4">Ridho Aulia' Rahman</h1>
             <p class="lead">Student</p>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,192L24,213.3C48,235,96,277,144,250.7C192,224,240,128,288,80C336,32,384,32,432,64C480,96,528,160,576,197.3C624,235,672,245,720,234.7C768,224,816,192,864,186.7C912,181,960,203,1008,208C1056,213,1104,203,1152,208C1200,213,1248,235,1296,245.3C1344,256,1392,256,1416,256L1440,256L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1" d="M0,192L24,213.3C48,235,96,277,144,250.7C192,224,240,128,288,80C336,32,384,32,432,64C480,96,528,160,576,197.3C624,235,672,245,720,234.7C768,224,816,192,864,186.7C912,181,960,203,1008,208C1056,213,1104,203,1152,208C1200,213,1248,235,1296,245.3C1344,256,1392,256,1416,256L1440,256L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path>
+        </svg>
     </section>
     <!-- End Jumbotron -->
 
@@ -70,7 +72,9 @@
                 </div>
             </div>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e2edff" fill-opacity="1" d="M0,192L24,202.7C48,213,96,235,144,202.7C192,171,240,85,288,85.3C336,85,384,171,432,202.7C480,235,528,213,576,181.3C624,149,672,107,720,112C768,117,816,171,864,213.3C912,256,960,288,1008,250.7C1056,213,1104,107,1152,64C1200,21,1248,43,1296,74.7C1344,107,1392,149,1416,170.7L1440,192L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#e2edff" fill-opacity="1" d="M0,192L24,202.7C48,213,96,235,144,202.7C192,171,240,85,288,85.3C336,85,384,171,432,202.7C480,235,528,213,576,181.3C624,149,672,107,720,112C768,117,816,171,864,213.3C912,256,960,288,1008,250.7C1056,213,1104,107,1152,64C1200,21,1248,43,1296,74.7C1344,107,1392,149,1416,170.7L1440,192L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path>
+        </svg>
     </section>
     <!-- End About Me -->
 
@@ -115,32 +119,37 @@
                 </div>
             </div>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,192L34.3,192C68.6,192,137,192,206,202.7C274.3,213,343,235,411,234.7C480,235,549,213,617,213.3C685.7,213,754,235,823,202.7C891.4,171,960,85,1029,74.7C1097.1,64,1166,128,1234,165.3C1302.9,203,1371,213,1406,218.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffffff" fill-opacity="1" d="M0,192L34.3,192C68.6,192,137,192,206,202.7C274.3,213,343,235,411,234.7C480,235,549,213,617,213.3C685.7,213,754,235,823,202.7C891.4,171,960,85,1029,74.7C1097.1,64,1166,128,1234,165.3C1302.9,203,1371,213,1406,218.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+        </svg>
     </section>
     <!-- End Projects -->
 
     <!-- Contact -->
     <section id="contact">
         <div class="container">
-            <div class="row text-center">
+            <div class="row text-center mb-3">
                 <div class="col">
                     <h2>Contact Me</h2>
                 </div>
             </div>
 
             <div class="row justify-content-center">
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <form>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="name">
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="email">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="mb-3">
+                            <label for="message" class="form-label">Pesan</label>
+                            <textarea class="form-control" id="message" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Kirim</button>
                     </form>
                 </div>
             </div>
