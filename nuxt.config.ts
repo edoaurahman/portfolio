9aef6f1985f6e553265d479e8be997dcd652432b
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2025-01-03 17:09:08 +0700

    feat: add i18n module and typedjs dependency to nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: [
-    'shadcn-nuxt',
-    '@nuxtjs/color-mode',
-    '@hypernym/nuxt-anime',
-    '@nuxtjs/tailwindcss',
-    '@nuxt/icon',
-    'nuxt-particles',
-    '@nuxt/fonts'
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "@hypernym/nuxt-anime",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/icon",
+    "nuxt-particles",
+    "@nuxt/fonts",
+    "@nuxtjs/i18n",
+    "nuxt-typedjs",
   ],
   shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: './components/ui'
+    prefix: "",
+    componentDir: "./components/ui",
   },
   colorMode: {
-    classSuffix: ''
+    classSuffix: "",
   },
   particles: {
     lazy: true,
-    mode: 'full' // 'full' | 'slim' | 'basic' | 'custom'
-  }
-})
+    mode: "full", // 'full' | 'slim' | 'basic' | 'custom'
+  },
+  i18n: {
+    locales: [
+      {
+        code: "en",
+        name: "English",
+      },
+      {
+        code: "id",
+        name: "Indonesia",
+      },
+    ],
+    defaultLocale: "en",
+    vueI18n: "./i18n.config.ts",
+  },
+});
