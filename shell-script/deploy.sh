41f70a5bb7da8975a1182872c857c6278d38dd54
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2025-01-05 18:32:51 +0700

    feat: add deployment script to automate build and server restart

---
+++ shell-script/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git pull
+export NODE_OPTIONS="--max-old-space-size=8192"
+bun run build
+pm2 restart all
+echo "Deployed successfully"
+ 
+ # The above script will pull the latest changes from the repository, build the project, restart the server, and print the message “Deployed successfully”. 
+ # To run the script, you need to give execute permission to the file. 
+ #$ chmod +x deploy.sh
